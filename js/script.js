c7b364c2ecf75cfc62296b47957c78c77c325df4
skf <conradmcgrifter@protonmail.com>
2021-10-04 17:44:29 -0400

    cleanup + small adjustments

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const MONTHLY_BUTTON = document.querySelector("#monthlyButton");
 
 const boxes = Array.from(document.querySelectorAll(".info--container"));
 const dots = Array.from(document.querySelectorAll(".dots"));
-const hours = document.querySelectorAll(".hours--wrap");
+const hours = Array.from(document.querySelectorAll(".hours--wrap"));
 
 const workHours = document.querySelector("#work-hours");
 const prevWorkHours = document.querySelector("#previous-work-hours");
@@ -65,29 +65,17 @@ const fetchData = () => {
 
             // work data
             const {
-                timeframes: {
-                    daily: work_Daily,
-                    weekly: work_Weekly,
-                    monthly: work_Monthly,
-                },
+                timeframes: { daily: work_Daily, weekly: work_Weekly, monthly: work_Monthly },
             } = json[0];
 
             // play data
             const {
-                timeframes: {
-                    daily: play_Daily,
-                    weekly: play_Weekly,
-                    monthly: play_Monthly,
-                },
+                timeframes: { daily: play_Daily, weekly: play_Weekly, monthly: play_Monthly },
             } = json[1];
 
             // study data
             const {
-                timeframes: {
-                    daily: study_Daily,
-                    weekly: study_Weekly,
-                    monthly: study_Monthly,
-                },
+                timeframes: { daily: study_Daily, weekly: study_Weekly, monthly: study_Monthly },
             } = json[2];
 
             // exercise data
@@ -101,11 +89,7 @@ const fetchData = () => {
 
             // social data
             const {
-                timeframes: {
-                    daily: social_Daily,
-                    weekly: social_Weekly,
-                    monthly: social_Monthly,
-                },
+                timeframes: { daily: social_Daily, weekly: social_Weekly, monthly: social_Monthly },
             } = json[4];
 
             // selfcare data
@@ -118,15 +102,7 @@ const fetchData = () => {
             } = json[5];
 
             // this function takes the object variables from the parsed JSON data to set the correct hours in the HTML. it also takes a string that displays the timeframe
-            const setData = (
-                work,
-                play,
-                study,
-                exercise,
-                social,
-                selfCare,
-                str
-            ) => {
+            const setData = (work, play, study, exercise, social, selfCare, str) => {
                 workHours.innerText = `${work.current}hrs`;
                 prevWorkHours.innerText = str + ` - ${work.previous}hrs`;
 
@@ -137,15 +113,13 @@ const fetchData = () => {
                 prevStudyHours.innerText = str + ` - ${study.previous}hrs`;
 
                 exerciseHours.innerText = `${exercise.current}hrs`;
-                prevExerciseHours.innerText =
-                    str + ` - ${exercise.previous}hrs`;
+                prevExerciseHours.innerText = str + ` - ${exercise.previous}hrs`;
 
                 socialHours.innerText = `${social.current}hrs`;
                 prevSocialHours.innerText = str + ` - ${social.previous}hrs`;
 
                 selfCareHours.innerText = `${selfCare.current}hrs`;
-                prevSelfCareHours.innerText =
-                    str + ` - ${selfCare.previous}hrs`;
+                prevSelfCareHours.innerText = str + ` - ${selfCare.previous}hrs`;
             };
 
             DAILY_BUTTON.addEventListener("click", () => {
